fab0652c77cd91152af9cd47f826afa119936473
solomon chigozie <solomon1chigozie@gmail.com>
2024-07-13 13:54:25 +0100

    update to provider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -24,7 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    const Text(
+                     const Text(
                       'You have pushed the button this many times:',
                     ),
                     Text(
